7ad7575299373ecc2769961c00408e332a9bed05
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-09-17 15:56:15 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const submitButton = document.getElementById('create-task-form').querySelector("input[type=submit]")
+  const taskDescription = document.getElementById('new-task-description')
+  const tasks = document.getElementById('tasks')
+
+  submitButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    var newTask = taskDescription.value
+    var list = document.createElement('li')
+    var textNode = document.createTextNode(newTask)
+    list.appendChild(textNode)
+    tasks.appendChild(list)
+  })
 });
